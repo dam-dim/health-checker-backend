d5e1f439b280b4fc2be5e792c39810248a7f6671
damdim <dam.dimov99@gmail.com>
2025-12-05 23:59:35 +0200

    Fixed error in server.js - import dotenv

--- src/middleware/api.auth.key.js
+++ src/middleware/api.auth.key.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const apiKeyAuth = (req, res, next) => {
-    const key = req.header('x-api-key');
+    const key = req.header(process.env.API_KEY_HEADER);
     if (!key || key !== process.env.API_KEY) {
         return res.status(401).json({ error: 'Unauthorized' });
     }
