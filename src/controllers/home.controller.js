95fe705abddea86aca95d8f1c93456050c515bbc
damdim <dam.dimov99@gmail.com>
2025-12-08 22:41:20 +0200

    added neon config

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,8 +3,6 @@ import {Router} from "express";
 const homeController = Router();
 
 homeController.get("/", (req, res) => {
-    console.log("ok...");
-    console.log(req);
     res.json({ status: 'ok', time: new Date() });
 });
 
